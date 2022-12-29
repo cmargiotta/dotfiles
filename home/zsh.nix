307fe4052c949f4138c3e6ce6a3a07525cd9d196
Carmine Margiotta <cmargiotta@posteo.net>
2022-12-29 21:07:57 +0100

    Update

--- home/zsh.nix
+++ home/zsh.nix
@@ -4,7 +4,7 @@
     enable = true;
     shellAliases = {
       nixos-apply = "sudo nixos-rebuild switch --flake ~/dotfiles";
-      nixos-update = "nix flake update ~/dotfiles && nixos-apply";
+      nixos-update = "pushd ~/dotfiles; source update.sh; popd";
       nixos-clean = "sudo nix-collect-garbage -d && sudo nix store optimise";
       ssh = "kitty +kitten ssh";
     };
