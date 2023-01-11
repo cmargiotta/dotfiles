cad0e033ef11896e5923bc7b913e08b572f79924
Carmine Margiotta <cmargiotta@posteo.net>
2023-01-11 08:17:20 +0100

    Fixed typo

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 wget https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/applications/editors/vscode/extensions/update_installed_exts.sh
 chmod +x update_installed_exts.sh
-./update_installed_exts.sh > home/vsocde-extensions.nix
+./update_installed_exts.sh > home/vscode-extensions.nix
 rm update_installed_exts.sh
 
 nix flake update ~/dotfiles && nixos-apply
