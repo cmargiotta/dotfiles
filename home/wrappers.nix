b3b53449a4d5c60ae59618bb48c5fae2f20936c2
Carmine Margiotta <cmargiotta@posteo.net>
2023-05-17 19:44:08 +0200

    Native wayland support added for vscode

--- home/wrappers.nix
+++ home/wrappers.nix
@@ -1,16 +1,6 @@
 { config, pkgs, nixpkgs, eww, ... }:
 {
   xdg.desktopEntries = {
-    code_x11 = {
-      name = "Visual Studio Code";
-      genericName = "Text Editor";
-      terminal = false;
-      categories = [ "Utility" "TextEditor" "Development" "IDE" ];
-      mimeType = [ "text/plain" "inode/directory" ];
-      icon = "code";
-      exec = "code --ozone-platform=x11";
-    };
-
     webcord = {
       name = "Webcord";
       genericName = "Discord and Fosscord client";
