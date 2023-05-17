401e92aebe2b208c2075b56964f2cae011e5b036
Carmine Margiotta <cmargiotta@posteo.net>
2023-05-18 01:01:37 +0200

    Wrapped mongodb compass (Electron...)

--- home/wrappers.nix
+++ home/wrappers.nix
@@ -1,6 +1,17 @@
 { config, pkgs, nixpkgs, eww, ... }:
 {
   xdg.desktopEntries = {
+    mongodb-compass = {
+      name = "MongoDB Compass";
+      genericName = "MongoDB Compass";
+      terminal = false;
+      categories = [ "Network" ];
+      mimeType = [ "x-scheme-handler/mongodb-compass" ];
+      icon = "mongodb-compass";
+      exec = "mongodb-compass --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --ignore-additional-command-line-flags";
+    };
+  };
+  xdg.desktopEntries = {
     webcord = {
       name = "Webcord";
       genericName = "Discord and Fosscord client";
