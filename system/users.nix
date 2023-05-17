37d35901c311611ed81802c0614ae04a2fcc1edc
Carmine Margiotta <cmargiotta@posteo.net>
2023-05-17 20:47:23 +0200

    Added user to podman group

--- system/users.nix
+++ system/users.nix
@@ -5,7 +5,7 @@
   users.users.nychtelios = {
     isNormalUser = true;
     home = "/home/nychtelios";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" "networkmanager" "podman" ];
     hashedPassword = (builtins.substring 0 106 (builtins.readFile ../secrets/nychtelios));
     shell = pkgs.zsh;
   };
