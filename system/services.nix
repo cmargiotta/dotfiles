b2bd9b98f4f67913e6c3cb46edfa8e00a32057d2
Carmine Margiotta <cmargiotta@posteo.net>
2023-07-03 14:51:05 +0200

    udev rules for jlink

--- system/services.nix
+++ system/services.nix
@@ -7,6 +7,11 @@
 
   services.dbus.packages = [ pkgs.gcr ];
 
+  # Permissions for JLink
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1366", ATTR{idProduct}=="0101", MODE="0666", GROUP="plugdev"
+  '';
+
   services.pipewire = {
     enable = true;
 
