00854c5c4a3d7d950bfc1322bca19a88a9a3e375
Carmine Margiotta <cmargiotta@posteo.net>
2023-07-07 08:54:29 +0200

    Added jtrace udev rule

--- system/services.nix
+++ system/services.nix
@@ -10,6 +10,7 @@
   # Permissions for JLink
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="1366", ATTR{idProduct}=="0101", MODE="0666", GROUP="plugdev"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1366", ATTR{idProduct}=="1020", MODE="0666", GROUP="plugdev"
   '';
 
   services.pipewire = {
