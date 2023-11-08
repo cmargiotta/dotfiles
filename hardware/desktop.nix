cd5654cef408f3d2d195c571c054f48bd1a3297b
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-08 09:18:05 +0100

    Nvidia is working better

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -10,7 +10,6 @@
       ./nvidia.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_5_10;
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,43 +1,23 @@
 { config, pkgs, lib, ... }:
-let
-  nvidiaPackage = config.boot.kernelPackages.nvidiaPackages.beta;
-in
 {
   config = {
-    environment.etc."gbm/nvidia-drm_gbm.so".source = "${nvidiaPackage}/lib/libnvidia-allocator.so";
-    environment.etc."egl/egl_external_platform.d".source = "/run/opengl-driver/share/egl/egl_external_platform.d/";
     hardware.opengl.enable = true;
     hardware.opengl.driSupport = true;
     hardware.opengl.driSupport32Bit = true;
     hardware.pulseaudio.support32Bit = true;
-    hardware.opengl.extraPackages = with pkgs; [
-      libva
-      nvidia-vaapi-driver
-    ];
-    environment.systemPackages = with pkgs; [
-      egl-wayland
-    ];
 
     boot.extraModprobeConfig = ''
-      options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"
+      options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3 nvidia_drm.modeset=1"
     '';
 
     hardware.nvidia.modesetting.enable = true;
-    hardware.nvidia.package = nvidiaPackage;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
     hardware.nvidia.powerManagement.enable = true;
-    hardware.nvidia.open = true;
+    hardware.nvidia.open = false;
     hardware.nvidia.nvidiaSettings = true;
 
     services.xserver = {
-      enable = true;
-      autorun = false;
       videoDrivers = [ "nvidia" ];
-      displayManager = {
-        gdm = {
-          wayland = true;
-          enable = true;
-        };
-      };
     };
   };
 }
