89799312b183a926196e26581c7725412b5391fd
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-08 21:07:32 +0100

    Added reboot alias

--- home/zsh.nix
+++ home/zsh.nix
@@ -10,6 +10,7 @@
       nixos-update = "nixos-clear && pushd ~/dotfiles; source update.sh; popd";
       nixos-clear = "sudo nix-collect-garbage -d && sudo nix store optimise";
       ssh = "kitty +kitten ssh";
+      reboot = "sudo systemctl reboot";
     };
     initExtra = "LOCALE_ARCHIVE=\"$(nix profile list | grep glibcLocales | tail -n1 | cut -d ' ' -f4)/lib/locale/locale-archive\"";
 
