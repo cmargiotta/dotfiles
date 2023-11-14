49e1ad1e7b4cfc350c1c85a47073d6d730cbeed9
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-14 15:09:37 +0100

    Update

--- home/vscode-extensions.nix
+++ home/vscode-extensions.nix
@@ -6,10 +6,34 @@
     sha256 = "0lbxsbhf7y3qfjyilgyhqw0yd2fa08qcikc8n58g6m71b4ybh3d7";
   }
   {
+    name = "cpp-helper";
+    publisher = "amiralizadeh9480";
+    version = "0.3.3";
+    sha256 = "1qhddlvs6zm2cpsh6rfp06nyps54pn88w1fany6rlyicnsllxbiq";
+  }
+  {
     name = "Nix";
     publisher = "bbenoist";
     version = "1.0.1";
     sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+  {
+    name = "vscode-tailwindcss";
+    publisher = "bradlc";
+    version = "0.11.30";
+    sha256 = "1xbcvs2i079xiah679cjczansfrgr2cp44xvydq04ndv1fyp4b6l";
+  }
+  {
+    name = "npm-intellisense";
+    publisher = "christian-kohler";
+    version = "1.4.5";
+    sha256 = "1av0iwym6j5l2a522ikyjkrsyp4q3sws1d7y9brm3gdgghd8aawn";
+  }
+  {
+    name = "path-intellisense";
+    publisher = "christian-kohler";
+    version = "2.8.5";
+    sha256 = "1ndffv1m4ayiija1l42m28si44vx9y6x47zpxzqv2j4jj7ga1n5z";
   }
   {
     name = "doxdocgen";
@@ -18,16 +42,22 @@
     sha256 = "1d95znf2vsdzv9jqiigh9zm62dp4m9jz3qcfaxn0n0pvalbiyw92";
   }
   {
+    name = "vscode-eslint";
+    publisher = "dbaeumer";
+    version = "2.4.2";
+    sha256 = "1g5mavks3m4fnn7wav659rdnd9f3lp7r96g8niad4g1vaj4xm23q";
+  }
+  {
     name = "githistory";
     publisher = "donjayamanne";
     version = "0.6.20";
     sha256 = "0x9q7sh5l1frpvfss32ypxk03d73v9npnqxif4fjwcfwvx5mhiww";
   }
   {
-    name = "nix-ide";
-    publisher = "jnoortheen";
-    version = "0.2.2";
-    sha256 = "1264027sjh9a112si0y0p3pk3y36shj5b4qkpsj207z7lbxqq0wg";
+    name = "search-node-modules";
+    publisher = "jasonnutter";
+    version = "1.3.0";
+    sha256 = "10kxx732cg60ncr4x1by6h1nab1a8xa4lh1rbhkwgs3qa44s8q2z";
   }
   {
     name = "vscode-clangd";
@@ -36,10 +66,10 @@
     sha256 = "0s7n3r3fkb6xh38yv8q0nj0js1dkxl53xhr1dvqia7gh71i6rsn8";
   }
   {
-    name = "mesonbuild";
-    publisher = "mesonbuild";
-    version = "1.16.3";
-    sha256 = "0bkyix67ssv0d9bkdpkgxk6kz9xljdrxa18ila1m93vgwx63h7gc";
+    name = "vscode-language-babel";
+    publisher = "mgmcdermott";
+    version = "0.0.40";
+    sha256 = "18pr0jg62ddb23b6pfq5fxr9mff2ylhw46apcd5616km0122swr5";
   }
   {
     name = "direnv";
@@ -50,8 +80,8 @@
   {
     name = "cpptools";
     publisher = "ms-vscode";
-    version = "1.18.1";
-    sha256 = "1lfzsy4zq3lav62bj9nlmdy5j3wpcd80hgd2aw609ma8a9v084jd";
+    version = "1.18.3";
+    sha256 = "1yr65w1ycgmj8amx0ycgdgvzz33s6w4si4ld4vbxry0ykrb9m9pv";
   }
   {
     name = "platformio-ide";
@@ -60,22 +90,22 @@
     sha256 = "0aii9ms2wzwfc5008c85fbz1k3ggqsqc29ahcjl7l2mrfhdh513w";
   }
   {
+    name = "vscode-sort-json";
+    publisher = "richie5um2";
+    version = "1.20.0";
+    sha256 = "1zcbdzsv6vv3zwx5ddbarqizs8s9s57dnf328waq8jgqyzjg31i6";
+  }
+  {
     name = "rust-analyzer";
     publisher = "rust-lang";
-    version = "0.4.1724";
-    sha256 = "078jn1ahqg4lgnz7rhr305183zz5p7fhm8hqksg3hq98lxb35sj3";
+    version = "0.4.1731";
+    sha256 = "12i6wmf6i8fhsbvi7xj9h8v6kgaxz8hbk284d0gs9ki73gqr3gfy";
   }
   {
-    name = "vscode-todo-highlight";
-    publisher = "wayou";
-    version = "1.0.5";
-    sha256 = "1sg4zbr1jgj9adsj3rik5flcn6cbr4k2pzxi446rfzbzvcqns189";
-  }
-  {
-    name = "json";
-    publisher = "ZainChen";
-    version = "2.0.2";
-    sha256 = "18pifg17w8zmv3kb74pjxwlyfssq66h3bzsqhgipzdl2mgqd0bcw";
+    name = "ReactSnippets";
+    publisher = "xabikos";
+    version = "2.4.0";
+    sha256 = "0gbs5pc9wrg7bcn1dakhfid1ancbz1ikz3n31hgawsgais5f931b";
   }
 ];
 }
