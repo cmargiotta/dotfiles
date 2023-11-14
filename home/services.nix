f8a6ac2a37c02ac0a03401c9ed447d0970c1820d
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-14 15:07:38 +0100

    Enabled poweraltertd

--- home/services.nix
+++ home/services.nix
@@ -1,11 +1,15 @@
 { config, pkgs, ... }:
 {
-  services.gpg-agent =
-    {
-      enable = true;
-      enableZshIntegration = true;
-      pinentryFlavor = "qt";
-      maxCacheTtl = 28800;
-      defaultCacheTtl = 28800;
-    };
+  services = {
+    gpg-agent =
+      {
+        enable = true;
+        enableZshIntegration = true;
+        pinentryFlavor = "qt";
+        maxCacheTtl = 28800;
+        defaultCacheTtl = 28800;
+      };
+
+    poweralertd.enable = true;
+  };
 }
