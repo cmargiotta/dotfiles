683ac4c482dcade519b42f40cbdccb86679c5f2f
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-16 00:19:43 +0100

    Added easyeffects and noisetorch

--- system/audio.nix
+++ system/audio.nix
@@ -11,18 +11,12 @@
     jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
-
     socketActivation = true;
   };
 
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      		bluez_monitor.properties = {
-      			["bluez5.enable-sbc-xq"] = true,
-      			["bluez5.enable-msbc"] = true,
-      			["bluez5.enable-hw-volume"] = true,
-      			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      		}
-      	'';
-  };
+  environment.systemPackages = with pkgs; [
+    easyeffects
+  ];
+
+  programs.noisetorch.enable = true;
 }
