93f3c69411cecdc25dab1d6609ea8f440683adfd
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-16 00:20:33 +0100

    Removed gamescope, it doesn't work well with nvidia

--- system/desktop.nix
+++ system/desktop.nix
@@ -3,8 +3,6 @@
   networking.networkmanager.enable = true;
   networking.wireless.enable = false;
 
-  services.blueman.enable = true;
-
   services.printing = {
     enable = true;
     logLevel = "debug";
@@ -12,22 +10,5 @@
     drivers = [
       pkgs.hplipWithPlugin
     ];
-  };
-
-  programs.steam.gamescopeSession = {
-    enable = true;
-    args = [
-      "--rt"
-      "--prefer-vk-device 8086:9bc4"
-      "-W 3440"
-      "-H 1440"
-      "-f"
-      "-e"
-    ];
-    env = {
-      ENABLE_VKBASALT = "1";
-      SDL_VIDEODRIVER = "x11";
-      GDK_BACKEND = "x11";
-    };
   };
 }
