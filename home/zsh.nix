39f55bf7053869973c8af1f3d98916910521621e
Carmine Margiotta <cmargiotta@posteo.net>
2023-11-28 21:33:00 +0100

    Added starship

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,10 +1,24 @@
 { config, pkgs, lib, ... }:
 {
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = (builtins.readFile ./config/starship.toml);
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
+
+    history = {
+      expireDuplicatesFirst = true;
+      extended = false;
+      ignoreDups = true;
+      save = 1000;
+    };
+
     shellAliases = {
       nixos-apply = "sudo nixos-rebuild switch --flake ~/dotfiles";
       nixos-update = "nixos-clear && pushd ~/dotfiles; source update.sh; popd";
@@ -12,15 +26,20 @@
       ssh = "kitty +kitten ssh";
       reboot = "sudo systemctl reboot";
     };
+
     initExtra = ''
       LOCALE_ARCHIVE=\"$(nix profile list | grep glibcLocales | tail -n1 | cut -d ' ' -f4)/lib/locale/locale-archive\"
-      neofetch
+      fastfetch
     '';
 
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "history" "history-substring-search" "man" ];
-      theme = "juanghurtado";
+      plugins = [
+        "git"
+        "history"
+        "history-substring-search"
+        "man"
+      ];
     };
   };
 }
