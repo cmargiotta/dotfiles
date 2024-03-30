94b26103866467b4c5340346bc2ce9581bfa05f2
Carmine Margiotta <cmargiotta@posteo.net>
2024-03-30 19:58:33 +0100

    Added hardware acceleration

--- chitubox/run.sh
+++ chitubox/run.sh
@@ -6,4 +6,4 @@ if [ -z "$(sudo docker images -q chitubox:done 2> /dev/null)" ]; then
     sudo docker load -i ~/dotfiles/chitubox/chitubox.tar.gz
 fi
 
-sudo docker run -e XDG_RUNTIME_DIR=/tmp -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ~/.local/share/chitubox2_0:/root/.local/share/chitubox2_0 -v ~:/mnt:rw -it --entrypoint /opt/CBD/CHITUBOX_Basic/CHITUBOX_Basic.sh chitubox:done --no-sandbox
+sudo docker run --device /dev/dri/card1:/dev/dri/card1 --device /dev/dri/renderD128:/dev/dri/renderD128  -e XDG_RUNTIME_DIR=/tmp -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ~/.local/share/chitubox2_0:/root/.local/share/chitubox2_0 -v ~:/mnt:rw -it --entrypoint /opt/CBD/CHITUBOX_Basic/CHITUBOX_Basic.sh chitubox:done --no-sandbox
