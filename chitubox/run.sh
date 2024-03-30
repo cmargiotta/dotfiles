085d54af2ef8e39157fdc17584d329968ed3566b
Carmine Margiotta <cmargiotta@posteo.net>
2024-03-30 21:06:25 +0100

    Added chitubox config

--- chitubox/run.sh
+++ chitubox/run.sh
@@ -6,4 +6,13 @@ if [ -z "$(sudo docker images -q chitubox:done 2> /dev/null)" ]; then
     sudo docker load -i ~/dotfiles/chitubox/chitubox.tar.gz
 fi
 
-sudo docker run --device /dev/dri/card1:/dev/dri/card1 --device /dev/dri/renderD128:/dev/dri/renderD128  -e XDG_RUNTIME_DIR=/tmp -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ~/.local/share/chitubox2_0:/root/.local/share/chitubox2_0 -v ~:/mnt:rw --entrypoint /opt/CBD/CHITUBOX_Basic/CHITUBOX_Basic.sh chitubox:done --no-sandbox
+sudo docker run \
+  --device /dev/dri/card1:/dev/dri/card1 \
+  --device /dev/dri/renderD128:/dev/dri/renderD128 \
+  -e XDG_RUNTIME_DIR=/tmp -e DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v ~/dotfiles/home/config/chitubox:/root/.local/share/chitubox2_0:rw \
+  -v ~:/mnt:rw \
+  --user 0:1000 \
+  --entrypoint /opt/CBD/CHITUBOX_Basic/CHITUBOX_Basic.sh \
+  chitubox:done --no-sandbox
