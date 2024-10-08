f1205216b80d436d82d881d532381b568bcc4430
Carmine Margiotta <email@cmargiotta.net>
2024-10-08 09:17:29 +0200

    Solved conflict

--- hardware/laptop.nix
+++ hardware/laptop.nix
@@ -36,7 +36,6 @@ in
       extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
       ];
