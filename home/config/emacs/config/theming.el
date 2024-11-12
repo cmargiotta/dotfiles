0484d212d8085cdd0a4a096e509a08d072ec2cf9
Carmine Margiotta <email@cmargiotta.net>
2024-11-12 12:41:49 +0100

    feat(emacs): increase line width limit

--- home/config/emacs/config/theming.el
+++ home/config/emacs/config/theming.el
@@ -32,7 +32,7 @@
     (add-hook hook #'whitespace-mode))
   (add-hook 'before-save-hook #'whitespace-cleanup)
   :config
-  (setq whitespace-line-column 80) ;; limit line length
+  (setq whitespace-line-column 140) ;; limit line length
   (setq whitespace-style '(face tabs empty trailing lines-tail)))
 
 ;; Enable ligatures in programming modes
