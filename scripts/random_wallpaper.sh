f56937abc0808a09363a271d6a822fd3ab7f439f
Carmine Margiotta <email@cmargiotta.net>
2024-12-13 23:46:33 +0100

    feat: New random wallpaper implementation

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -3,17 +3,28 @@
 monitors=$(hyprctl monitors -j | jq -r '.[] | .name')
 
 random_wallpaper_file() {
-    fd --type file --extension jpg '.*' ~/.wallpapers/ | shuf -n 1
+    w=$1
+    h=$2
+    path="$HOME/.wallpapers/${w}x$h"
+
+    fd --type file --extension jpg --extension jpeg '.*' "$path" | shuf -n 1
 }
 
-wallpaper=$(random_wallpaper_file)
-wal -i $wallpaper
-
 for monitor in $monitors; do
-    hyprctl hyprpaper preload $wallpaper
-    hyprctl hyprpaper wallpaper `echo "$monitor,$wallpaper"`
+    echo "$monitor"
+    width=$(hyprctl monitors -j | jq -r ".[] | select(.name == \"$monitor\") | .width")
+    height=$(hyprctl monitors -j | jq -r ".[] | select(.name == \"$monitor\") | .height")
 
-    wallpaper=$(random_wallpaper_file)
+    echo "Detected resolution: ${width}x${height}"
+
+    wallpaper=$(random_wallpaper_file "$width" "$height")
+
+    echo "Loading $wallpaper"
+
+    hyprctl hyprpaper preload "$wallpaper"
+    hyprctl hyprpaper wallpaper "${monitor},${wallpaper}"
+
+    wal -i "$wallpaper"
 done
 
-ln -sf $(random_wallpaper_file) ~/.lock_wallpaper.jpg
+ln -sf "$(random_wallpaper_file "lockscreen")" ~/.lock_wallpaper.jpg
